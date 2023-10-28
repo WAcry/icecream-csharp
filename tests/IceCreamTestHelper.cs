69f079a1463f2ed58072bd4773e511a1cff5af5f
David Zhang <david.zhang@roblox.com>
2023-10-28 01:50:40 -0700

    v0.6 - refactor and add unit tests

---
+++ tests/IceCreamTestHelper.cs
@@ -0,0 +1,23 @@
+﻿using icecream;
+
+namespace tests
+{
+    public static class IceCreamTestHelper
+    {
+        public const string FileName = "IceCreamTestHelper.cs";
+        public const int IcLineNum = 15;
+        public const string IcFuncName = "Ic()";
+        public const int FormatLineNum = 20;
+        public const string FormatFuncName = "IceFormat()";
+
+        public static T Ic<T>(T arg, string label = null)
+        {
+            return label == null ? arg.ic() : arg.ic(label);
+        }
+
+        public static string IceFormat<T>(T arg, string label = null)
+        {
+            return label == null ? arg.IceFormat() : arg.IceFormat(label);
+        }
+    }
+}
